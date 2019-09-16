f7f8a69ee7e029375d72d8a9affbcaa4f32d1f17
EliChang <eliflichang@gmail.com>
2019-09-16 18:10:41 +0800

    Update query

--- js/query.js
+++ js/query.js
@@ -1,4 +1,6 @@
 
+var lastUids = []
+
 function Ajax(conf, callback) {
     chrome.runtime.sendMessage(conf, callback)
 }
@@ -9,19 +11,21 @@ $(document).ready(function() {
 
 var intervalQuery = () => {
     var uids = getlistUid();
-    if (uids.length <= 0) {
-        setTimeout(intervalQuery, 5000);
-    }    
+    if (uids.length > 0) {
+        if (lastUids.toString() != uids.toString()) {
+            lastUids = uids;
+            console.log('lastUids changed: \n\t' + lastUids);
+        }
+    }
+    setTimeout(intervalQuery, 2000);
 }
 
 function getlistUid() {
     var listUid = [];
     $('#list-container>.list-item .shop-name').each(function (i, it) {
         var par = $(it).parents('.list-item');
-        if (par && par.attr('GaoHao-IsSearch') === "true") return;
         var uid = $(it).attr('trace-uid');
         listUid.push(uid)
     });
-    console.log('getlistUid: ' + listUid);
     return listUid;
 }
