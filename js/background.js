a96b5dd24ce25537dc0983270940616d9569645a
eli <13095451128@163.com>
2019-09-16 23:42:58 +0800

    Commit at midnight of 19/9/16

--- js/background.js
+++ js/background.js
@@ -1,21 +1,22 @@
-
 var saveLoginInfo = null;
 var config = {
-	activepage: 'https://s.taobao.com',
+    activepage: 'https://s.taobao.com',
     httpHome: 'https://s.taobao.com'
 }
 
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostEquals: config.activepage},})
-			],
-			actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {
+                    hostEquals: config.activepage
+                },
+            })],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 
-	console.log('EXTWANG started.');
+    console.log('EXTWANG started.');
 })
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
@@ -23,7 +24,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (typeof request === 'object') {
         retFun = request
     } else {
-        retFun = JSON.parse(request);
+        retFun = eval('(' + request + ')');
     }
 
     if (retFun && retFun.type) {
@@ -35,20 +36,49 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                 async: false,
                 data: conf.data,
                 dataType: conf.dataType,
-                success: function (data) {
-                    sendResponse({type:"success", data:data, config:config})
+                success: function(data) {
+                    sendResponse({
+                        type: "success",
+                        data: data,
+                        config: config
+                    })
                 },
-                error: function (data) {
-                    if (data.responseText && data.responseText.indexOf('后台登录') > -1) {
-                    } else {
-                        sendResponse({type:"error",data:data,info:info,config:config})
+                error: function(data) {
+                    if (data.responseText && data.responseText.indexOf('后台登录') > -1) {} else {
+                        sendResponse({
+                            type: "error",
+                            data: data,
+                            info: info,
+                            config: config
+                        })
                     }
                 }
             })
         } else if (retFun.type === 'fake_ajax') {
-    		console.log('elifli:\n' + retFun);
+            sendResponse({
+                type: "success",
+                data: loadFakeData(retFun.conf.data.listUid),
+                config: config
+            });
+        } else if (retFun.type === 'log') {
+            console.log(retFun.log);
         }
     } else {
         sendResponse(JSON.stringify(retFun));
     }
 })
+
+function loadFakeData(uids) {
+    var data = [];
+    uids.forEach(function(uid) {
+        var ele = {
+            Uid: uid,
+            Company: 'AvocadoEli',
+            RegDate: '2102',
+            Mobile: '19839489304',
+            Phone: '010-3874623'
+        };
+        data.push(ele);
+    });
+    return data;
+}
